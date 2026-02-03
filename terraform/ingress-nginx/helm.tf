961a8d578db9e7c113e54d8c24b185209bac5326
SUDO <sudo-coder@users.noreply.github.com>
2026-02-04 03:40:22 +0700

    Ingress Helm Issue Fixed 3.0

--- terraform/ingress-nginx/helm.tf
+++ terraform/ingress-nginx/helm.tf
@@ -3,8 +3,8 @@ resource "helm_release" "nginx_ingress" {
   namespace = kubernetes_namespace.ingress.metadata[0].name
 
   repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx/ingress-nginx"
-  version    = "4.10.1"
+  chart      = "ingress-nginx"
+  version    = "4.11.3"
 
   create_namespace = false
   cleanup_on_fail  = true
