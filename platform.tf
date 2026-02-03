6eb702e15fec5ed0186d396130d6cfaa08ed1046
SUDO <sudo-coder@users.noreply.github.com>
2026-02-03 17:02:02 +0700

    Agro Setup 1.0 & Added Module Loader

---
+++ platform.tf
@@ -0,0 +1,28 @@
+# Platform Bootstrap Modules
+# Order:
+# 1. ingress-nginx
+# 2. cert-manager
+# 3. argocd
+
+module "ingress_nginx" {
+  source = "./ingress-nginx"
+
+  # Optional: if ingress module needs cluster info later
+  # depends_on = []
+}
+
+module "cert_manager" {
+  source = "./cert-manager"
+
+  depends_on = [
+    module.ingress_nginx
+  ]
+}
+
+module "argocd" {
+  source = "./argocd"
+
+  depends_on = [
+    module.cert_manager
+  ]
+}
